5276e45f299117c8cb0ad8f69b26e4df7b5dd903
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-10-23 16:05:15 -0300

    Beauty

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,7 +5,7 @@ import chess.ChessPiece;
 public class UI {
 
 	public static void printBoard(ChessPiece[][] pieces) {
-		System.out.println("  a b c d e f g h");
+		System.out.println("+ a b c d e f g h +");
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
@@ -14,7 +14,7 @@ public class UI {
 			System.out.print(8 - i);
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.println("+ a b c d e f g h +");
 	}
 	
 	private static void printPiece(ChessPiece piece) {
